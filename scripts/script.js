3cf95c4bcfbab2bd9a1dded82f3e24d2d0dd3b74
olala <123945379+ip-repo@users.noreply.github.com>
2024-07-07 15:06:11 +0300

    Updated:./scripts/script.js added url check for url-input

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,20 @@
 const fileTypeFromDropDown = document.getElementById('file-type')
 const fileUploadInput = document.getElementById('file-upload')
 const urlInput = document.getElementById('url-input')
-
+const loadButton = document.getElementById('load-button')
 // DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', function () {
     urlInput.style.display = 'none';
     fileTypeFromDropDown.value = "local";
+    loadButton.disabled = true
+
 });
 
 
 // Event listener for dropdown changes
 fileTypeFromDropDown.addEventListener('change', function(){
     const selectedOption = fileTypeFromDropDown.value;
+    console.log("New selection: ", selectedOption)
     // Show/hide input fields based on the selected option
     if (selectedOption === 'local'){
         fileUploadInput.style.display ='block';
@@ -23,3 +26,31 @@ fileTypeFromDropDown.addEventListener('change', function(){
 
     }
 });
+
+urlInput.addEventListener('input', function (){
+    const url = urlInput.value.trim();
+    var validateUrl = isValidUrl(url);
+    loadButton.disabled = !validateUrl;
+    if (validateUrl === true) {
+        console.log("Load button active");
+
+    }else {
+        console.log("Load button not active");
+    }
+});
+
+
+
+
+
+// Helper function to validate URLs
+function isValidUrl(url) {
+    try {
+        new URL(url);
+        console.log("Valid url: " + url)
+        return true
+    } catch(error) {
+        console.log('Invalid url: ' + error)
+        return false;
+    }
+}
