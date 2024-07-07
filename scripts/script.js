116550319523e59c21a7e9cc876d07b172447327
olala <123945379+ip-repo@users.noreply.github.com>
2024-07-07 16:10:10 +0300

    Updated:./scripts/script.js - Added load button event listener now the audio file or url can loaded into the audio player and played. Added comments as well.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
-//Get references to the input elements
+// Get references to the input elements
 const fileTypeFromDropDown = document.getElementById('file-type')
 const fileUploadInput = document.getElementById('file-upload')
 const urlInput = document.getElementById('url-input')
 const loadButton = document.getElementById('load-button')
+const audioPlayer = document.querySelector('.audio-player')
+
 // DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', function () {
     urlInput.style.display = 'none';
     fileTypeFromDropDown.value = "local";
-    loadButton.disabled = true
+    loadButton.disabled = true;
 
 });
 
@@ -25,16 +27,16 @@ fileTypeFromDropDown.addEventListener('change', function(){
         // Hide url
         urlInput.style.display = 'none';
         // Disable load button
-        loadButton.disabled = true
+        loadButton.disabled = true;
     } else if (selectedOption === 'url') {
         // Clear file upload button
-        fileUploadInput.value = ''
+        fileUploadInput.value = '';
         // hide file upload button
         fileUploadInput.style.display ='none';
         // Show url input 
         urlInput.style.display = 'block';
         // Disable load button
-        loadButton.disabled = true
+        loadButton.disabled = true;
 
     }
 });
@@ -51,9 +53,9 @@ urlInput.addEventListener('input', function (){
         console.log("Load button not active");
     }
 });
-
+// Event listener for file upload input
 fileUploadInput.addEventListener('change', function (){
-    // If a file is chosed then activate load button
+    // If a file is choosed then activate load button
     loadButton.disabled = !fileUploadInput.files[0];
     if (loadButton.disabled === false) {
         console.log("File Loaded\n",fileUploadInput.files[0]);
@@ -62,7 +64,53 @@ fileUploadInput.addEventListener('change', function (){
     }
 });
 
+// Event listener for load buttons
+loadButton.addEventListener('click', function () {
+    // Get dropdown selected value
+    const selectedOption = fileTypeFromDropDown.value;
+    // Audio source will hold the file as url object
+    let audioSource;
 
+    if (selectedOption === 'local'){
+        // The local file to play
+        const file = fileUploadInput.files[0];
+        if (file) {
+            // Create url object
+            audioSource = URL.createObjectURL(file);
+            // Load audio source to player
+            audioPlayer.src = audioSource;
+        } else {
+            // The file was loaded and load button clicked but something went wrong
+            alert("something went wrong")
+            // Clear file upload
+            fileUploadInput.value = '';
+            // clear url input
+            urlInput.value ='';
+            // Disable load button
+            loadButton.disabled = true;
+        }
+        
+    } else if (selectedOption == "url"){
+        // Clean url
+        const url = urlInput.value.trim()
+        if (isValidUrl(url)){
+            // Set web url as audio source
+            audioSource = url;
+            // Load audio source to player
+            audioPlayer.src = audioSource;
+        } else {
+            // The file was loaded and load button clicked but something went wrong
+            alert("something went wrong.")
+            // Clear file upload
+            fileUploadInput.value = '';
+            // clear url input
+            urlInput.value ='';
+            // Disable load button
+            loadButton.disabled = true;
+        }
+    }
+
+});
 
 
 // Helper function to validate URLs
